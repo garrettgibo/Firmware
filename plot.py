69a606dddbfeb07e6d1766c2caa1c866c32950fc
Manuel <Manuel>
2020-11-22 18:46:39 -0800

    edited python script for acs thrust

--- plot.py
+++ plot.py
@@ -2,9 +2,11 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 my_data = np.genfromtxt('build/px4_sitl_default/tmp/data.csv', delimiter=',')
-plt.title("actuator 0")
+plt.title("Roll")
+plt.xlabel("Time")
 plt.plot(my_data[:,0], marker='o')
 plt.figure()
-plt.title("actuator 1")
+plt.title("Pitch")
+plt.xlabel("Time")
 plt.plot(my_data[:,1], marker='o')
 plt.show()
